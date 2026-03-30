3f0cf5cf7cad7d24f1315c30c38f5a44fe76a852
twoehrer <cores163@gmail.com>
2026-03-30 17:54:10 +0200

    clean up part 1

--- models/neural_odes.py
+++ models/neural_odes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@author: borjangeshkovski (adapted from https://github.com/EmilienDupont/augmented-neural-odes)
+adapted from https://github.com/EmilienDupont/augmented-neural-odes
 """
 ##------------#
 import torch
